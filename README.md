ad8b082e26e4d42c6d074f1db9d72845c301fee5
T4skFoRcE <t4skforce@tk45nb.neumair.net>
2017-12-24 15:48:01 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# epd2in13
-2.13inch e-Paper HAT - Implements for e-paper library
+# About
+This repo was created to provide a convenient way to install GDEM0213E26LT 2.13" e-paper library for Raspberry Pi.
+The original code is provided by [Waveshare GitHub](https://github.com/soonuse/epd-library-python).
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(name='epd2in13',
+      version='0.1',
+      description='2.13inch e-Paper HAT - Implements for e-paper library',
+      url='https://github.com/t4skforce/edp2in13',
+      author='Yehui from Waveshare',
+      author_email='service@waveshare.com',
+      license='GPL',
+      packages=['epd2in13'],
+      install_requires=[
+          'RPi.GPIO',
+          'epdif',
+          'Pillow'
+      ],
+      classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3"
+      ],
+      zip_safe=False)
